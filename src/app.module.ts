2a3900198a5bac9e76b6ddb59ecc02631c74e209
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-11-12 00:42:36 +0900

    1. 프로젝트 환경 설정 다음과 같은 라이브러리가 추가되었습니다.
    ● class-transformer & class-validator
    ● morgan
    ● swagger
    ● passport-jwt & passport-local
    ● typeorm mysql2 & typeorm sqlite3
    ● config
    ● bcrypt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MorganInterceptor, MorganModule } from "nest-morgan";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService]
+	imports: [
+		// TypeOrmModule.forRoot({
+		// 	type: "sqlite",
+		// 	database: ":memory:",
+		// 	entities: [],
+		// 	synchronize: true
+		// }),
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		TypeOrmModule.forRoot({
+			type: "mysql",
+			host: process.env.DB_HOST,
+			port: 3306,
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			entities: [],
+			synchronize: true
+		}),
+		MorganModule
+	],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: MorganInterceptor("combined")
+		}
+	]
 })
 export class AppModule {}
